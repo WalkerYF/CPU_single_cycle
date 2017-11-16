dcd1a70eb15ec73ccdba4432ce4fb418e5bdaa73
wyf <walkeryf@yeah.net>
2017-11-16 08:50:46 +0800

    add ALU32_tb RegFile_tb

--- src/ALU32.v
+++ src/ALU32.v
@@ -24,17 +24,20 @@ module ALU32(
         input [31:0] rega,
         input [31:0] regb,
         output reg [31:0] result,
+        output sign,
         output zero
         );
     assign zero = (result == 0) ? 1 : 0;
+    assign sign = result[31];
     always @(ALUopcode or rega or regb) begin
         case (ALUopcode)
             3'b000 : result = rega + regb;
             3'b001 : result = rega - regb;
-            3'b010 : result = rega & regb;
+            3'b010 : result = regb << rega;
             3'b011 : result = rega | regb;
-            3'b100 : result = (rega < regb) ? 1 : 0; // 不带符号比较
-            3'b101 : 
+            3'b100 : result = rega & regb;
+            3'b101 : result = (rega < regb) ? 1 : 0; // 不带符号比较
+            3'b110 : 
                 begin                           // 带符号比较
                     if (rega < regb && ((rega[31] == 0 && regb[31] == 0) ||
                              (rega[31] == 1 && regb[31] == 1))) result = 1;
@@ -42,6 +45,8 @@ module ALU32(
                     else if (rega[31] == 1 && regb[31] == 0) result = 1;
                     else result = 0;
                 end 
+            3'b111:
+                    result = rega ^ regb;
             default : begin
                     result = 8'h00000000;
                     $display(" no match");
--- src/RegFile.v
+++ src/RegFile.v
@@ -21,20 +21,22 @@
 
 
 module RegFile(
-    input CLK, input RST, input RegWre,
+    input CLK, 
+    input RegWre,// 寄存器写使能端
     input [4:0] ReadReg1,
     input [4:0] ReadReg2,
-    input [4:0] WriteReg,
-    input [31:0] WriteData,
+    input [4:0] WriteReg, // 写寄存器号
+    input [31:0] WriteData, // 写寄存器的数据
     output [31:0] ReadData1,
-    output [31:0] ReadData2
+    output [31:0] ReadData2,
+    input RST // 清零，重置
 );
     reg [31:0] regFile[1:31]; // 寄存器定义必须用 reg 类型
     integer i;
-    assign   ReadData1 = (ReadReg1 == 0) ? 0 : regFile[ReadReg1]; // 读寄存器数据
+    assign   ReadData1 = (ReadReg1 == 0) ? 0 : regFile[ReadReg1]; // 读寄存器数据 // 可随时读
     assign   ReadData2 = (ReadReg2 == 0) ? 0 : regFile[ReadReg2];
 
-    always @ (negedge CLK or negedge RST) begin   // 必须用时钟边沿触发
+    always @ (negedge CLK or negedge RST) begin   // 必须用时钟边沿触发 // 时钟下降沿
         if (RST==0) begin
             for(i=1;i<32;i=i+1) regFile[i] <= 0;
         end
